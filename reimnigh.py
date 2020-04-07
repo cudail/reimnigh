9c1806921b388c54d59ebdd1c7cd2bbef83a5423
Caoimhe Ní Chaoimh <caoimhe@oakreef.ie>
2020-04-07 22:50:05 +0100

    🐛 adjusting logic for determining 1st or 2nd conjugation

--- reimnigh.py
+++ reimnigh.py
@@ -521,13 +521,13 @@ céad_réimniú_igh.m_coinn.céad_phearsa.iorla.mumhan = Leagan(deireadh_tháite
 
 
 def cén_réimniú(briathar:str)->Réimniú:
-	if comhair_siollaí(briathar) == 1:
-		if briathar[-3:] == 'igh' and briathar[-4:] != 'éigh':
-			return céad_réimniú_igh
-		return céad_réimniú
-	elif len(briathar) > 2 and briathar[-3:] in ['áil', 'áin', 'óil']:
-		return céad_réimniú
-	return dara_réimniú
+	if comhair_siollaí(briathar) > 1:
+		if briathar[-3:] == 'igh' or briathar[-2:] in ['ir', 'il', 'in']:
+			if briathar[-3:] not in ['áil', 'áin', 'óil']:
+				return dara_réimniú
+	if briathar[-3:] == 'igh' and briathar[-4:] not in ['éigh', 'óigh']:
+		return céad_réimniú_igh
+	return céad_réimniú
 
 def priontáil_toradh(toradh:List):
 	leithid_colún={}
